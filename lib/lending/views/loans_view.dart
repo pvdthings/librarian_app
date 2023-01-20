fefd3e7063d17a92966fdadb172b29cb1637d1ae
Dillon Fagan <dillonfagan@pm.me>
2023-01-20 18:01:24 -0500

    Add dummy list of loans to LoansView

--- lib/lending/views/loans_view.dart
+++ lib/lending/views/loans_view.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:librarian_app/views/placeholder_view.dart';
 
 class LoansView extends StatelessWidget {
   const LoansView({super.key});
 
+  static final List<Map<String, String>> _loans = [
+    {
+      "name": "Alice",
+      "things": "Chainsaw, Shovel, Rubber Gloves",
+      "due": "Today",
+    },
+    {
+      "name": "Bob",
+      "things": "Coventry-opoly",
+      "due": "10/31",
+    }
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const PlaceholderView(title: "View Current Loans");
+    return ListView.builder(
+      itemCount: _loans.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(_loans[index]["name"] as String),
+          subtitle: Text(_loans[index]["things"] as String),
+          trailing: Chip(
+            label: Text(_loans[index]["due"] as String),
+          ),
+          tileColor: (index % 2 == 0) ? null : Colors.blueGrey[50],
+          hoverColor: Colors.blueGrey[100],
+        );
+      },
+    );
   }
 }
