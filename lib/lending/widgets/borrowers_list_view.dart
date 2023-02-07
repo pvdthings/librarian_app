2bc96a5c4eda19f64deca0b035ef45ed73cc75ae
Dillon Fagan <dillonfagan@pm.me>
2023-02-07 10:15:41 -0500

    Add field for filtering borrowers

--- lib/lending/widgets/borrowers_list_view.dart
+++ lib/lending/widgets/borrowers_list_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:librarian_app/lending/models/borrowers_model.dart';
 import 'package:provider/provider.dart';
 
-class BorrowersListView extends StatelessWidget {
+class BorrowersListView extends StatefulWidget {
   const BorrowersListView({
     super.key,
     required this.onTapBorrower,
@@ -11,38 +11,68 @@ class BorrowersListView extends StatelessWidget {
   final void Function(Borrower borrower) onTapBorrower;
 
   @override
+  State<BorrowersListView> createState() => _BorrowersListViewState();
+}
+
+class _BorrowersListViewState extends State<BorrowersListView> {
+  final _searchController = TextEditingController();
+  String? _searchText;
+
+  @override
   Widget build(BuildContext context) {
-    return Consumer<BorrowersModel>(
-      builder: (context, model, child) {
-        final borrowers = model.all.toList();
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            onChanged: (value) {
+              setState(() => _searchText = value.toLowerCase());
+            },
+            controller: _searchController,
+            decoration: const InputDecoration(
+              hintText: "Search...",
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+        Consumer<BorrowersModel>(
+          builder: (context, model, child) {
+            var borrowers = model.all.toList();
 
-        return ListView.builder(
-          itemCount: borrowers.length,
-          itemBuilder: (context, index) {
-            final b = borrowers[index];
+            if (_searchText != null) {
+              borrowers = borrowers
+                  .where((b) => b.name.toLowerCase().contains(_searchText!))
+                  .toList();
+            }
 
-            return ListTile(
-              title: Text(b.name),
-              subtitle: b.active
-                  ? const Text(
-                      "Active",
-                      style: TextStyle(
-                        color: Colors.green,
-                      ),
-                    )
-                  : const Text(
-                      "Inactive",
-                      style: TextStyle(
-                        color: Colors.orange,
-                      ),
-                    ),
-              // tileColor: (index % 2 == 0) ? null : Colors.blueGrey[50],
-              // hoverColor: Colors.grey[100],
-              onTap: () => onTapBorrower(b),
+            return ListView.builder(
+              itemCount: borrowers.length,
+              itemBuilder: (context, index) {
+                final b = borrowers[index];
+
+                return ListTile(
+                  title: Text(b.name),
+                  subtitle: b.active
+                      ? const Text(
+                          "Active",
+                          style: TextStyle(
+                            color: Colors.green,
+                          ),
+                        )
+                      : const Text(
+                          "Inactive",
+                          style: TextStyle(
+                            color: Colors.orange,
+                          ),
+                        ),
+                  onTap: () => widget.onTapBorrower(b),
+                );
+              },
+              shrinkWrap: true,
             );
           },
-        );
-      },
+        ),
+      ],
     );
   }
 }
