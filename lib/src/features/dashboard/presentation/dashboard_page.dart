ca335f6368ce4b93af9428ab94ce61d274553c86
Dillon Fagan <dillonfagan@pm.me>
2023-05-30 09:36:55 -0400

    Hook up mobile loans layout to dashboard page

--- lib/src/features/dashboard/presentation/dashboard_page.dart
+++ lib/src/features/dashboard/presentation/dashboard_page.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:librarian_app/src/features/dashboard/presentation/desktop_layout.dart';
+import 'package:librarian_app/src/features/loans/presentation/loans_mobile_layout.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: DesktopLayout());
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final isMobile = constraints.maxWidth < 1100;
+
+        return Scaffold(
+            body: isMobile ? const LoansMobileLayout() : const DesktopLayout());
+      },
+    );
   }
 }
