761d60374a7ac6162cdc7e534fb94e51ed5ca862
Dillon Fagan <dillonfagan@pm.me>
2023-07-04 07:42:35 -0400

    Validate required thing name

--- lib/src/features/inventory/widgets/create_thing_dialog.dart
+++ lib/src/features/inventory/widgets/create_thing_dialog.dart
@@ -4,6 +4,7 @@ import 'package:librarian_app/src/features/common/widgets/input_decoration.dart'
 class CreateThingDialog extends StatelessWidget {
   CreateThingDialog({super.key, this.onCreate});
 
+  final _formKey = GlobalKey<FormState>();
   final void Function(String name, String? spanishName)? onCreate;
 
   final _name = TextEditingController();
@@ -15,63 +16,76 @@ class CreateThingDialog extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.all(16),
         child: IntrinsicWidth(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'New Thing',
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    icon: const Icon(Icons.close),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: _name,
-                decoration: inputDecoration.copyWith(
-                  labelText: 'Name',
-                  icon: const Icon(Icons.build),
-                  constraints: const BoxConstraints(minWidth: 500),
+          child: Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'New Thing',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: const Icon(Icons.close),
+                    ),
+                  ],
                 ),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: _spanishName,
-                decoration: inputDecoration.copyWith(
-                  labelText: 'Name (Spanish)',
-                  icon: const Icon(Icons.build),
-                  constraints: const BoxConstraints(minWidth: 500),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _name,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Name is required';
+                    }
+
+                    return null;
+                  },
+                  decoration: inputDecoration.copyWith(
+                    labelText: 'Name',
+                    icon: const Icon(Icons.build),
+                    constraints: const BoxConstraints(minWidth: 500),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 32),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  OutlinedButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text('Cancel'),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _spanishName,
+                  decoration: inputDecoration.copyWith(
+                    labelText: 'Name (Spanish)',
+                    icon: const Icon(Icons.build),
+                    constraints: const BoxConstraints(minWidth: 500),
                   ),
-                  const SizedBox(width: 16),
-                  FilledButton(
-                    onPressed: () {
-                      onCreate?.call(_name.text, _spanishName.text);
-                    },
-                    child: const Text('Create'),
-                  ),
-                ],
-              ),
-            ],
+                ),
+                const SizedBox(height: 32),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    OutlinedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Cancel'),
+                    ),
+                    const SizedBox(width: 16),
+                    FilledButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          onCreate?.call(_name.text, _spanishName.text);
+                        }
+                      },
+                      child: const Text('Create'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
