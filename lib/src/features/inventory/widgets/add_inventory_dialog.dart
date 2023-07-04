ba886aebd808d84a0291512f213c7c2ff5ec6f30
Dillon Fagan <dillonfagan@pm.me>
2023-07-04 07:57:13 -0400

    Input validation for AddInventoryDialog

--- lib/src/features/inventory/widgets/add_inventory_dialog.dart
+++ lib/src/features/inventory/widgets/add_inventory_dialog.dart
@@ -11,6 +11,8 @@ class AddInventoryDialog extends StatelessWidget {
     int quantity,
   )? onCreate;
 
+  final _formKey = GlobalKey<FormState>();
+
   final _quantity = TextEditingController(text: '1');
   final _brand = TextEditingController();
   final _estimatedValue = TextEditingController();
@@ -22,91 +24,117 @@ class AddInventoryDialog extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.all(16),
         child: IntrinsicWidth(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Add Items',
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    icon: const Icon(Icons.close),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: _quantity,
-                decoration: inputDecoration.copyWith(
-                  labelText: 'Quantity',
-                  icon: const Icon(Icons.numbers),
-                  constraints: const BoxConstraints(maxWidth: 150),
-                ),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: _brand,
-                decoration: inputDecoration.copyWith(
-                  labelText: 'Brand',
-                  icon: const Icon(Icons.label),
-                  constraints: const BoxConstraints(minWidth: 500),
-                ),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: _estimatedValue,
-                decoration: inputDecoration.copyWith(
-                  labelText: 'Estimated Value',
-                  icon: const Icon(Icons.currency_exchange),
-                  prefixText: '\$',
-                  constraints: const BoxConstraints(minWidth: 500),
-                ),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: _description,
-                decoration: inputDecoration.copyWith(
-                  labelText: 'Description',
-                  icon: const Icon(Icons.description),
-                  constraints: const BoxConstraints(minWidth: 500),
-                ),
-              ),
-              const SizedBox(height: 32),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  OutlinedButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text('Cancel'),
-                  ),
-                  const SizedBox(width: 16),
-                  FilledButton(
-                    onPressed: () async {
-                      await onCreate?.call(
-                        _brand.text,
-                        _description.text,
-                        double.tryParse(_estimatedValue.text),
-                        int.tryParse(_quantity.text) ?? 1,
-                      );
-
-                      await Future.delayed(Duration.zero, () {
+          child: Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Add Items',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    IconButton(
+                      onPressed: () {
                         Navigator.of(context).pop();
-                      });
-                    },
-                    child: const Text('Create'),
+                      },
+                      icon: const Icon(Icons.close),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _quantity,
+                  validator: (value) {
+                    return value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null
+                        ? 'Quantity must be a valid integer'
+                        : null;
+                  },
+                  keyboardType: TextInputType.number,
+                  decoration: inputDecoration.copyWith(
+                    labelText: 'Quantity',
+                    icon: const Icon(Icons.numbers),
+                    constraints: const BoxConstraints(maxWidth: 300),
                   ),
-                ],
-              ),
-            ],
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _brand,
+                  decoration: inputDecoration.copyWith(
+                    labelText: 'Brand',
+                    icon: const Icon(Icons.label),
+                    constraints: const BoxConstraints(minWidth: 500),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _estimatedValue,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return null;
+                    }
+
+                    return double.tryParse(value) == null
+                        ? 'Invalid value'
+                        : null;
+                  },
+                  keyboardType: TextInputType.number,
+                  decoration: inputDecoration.copyWith(
+                    labelText: 'Estimated Value',
+                    icon: const Icon(Icons.currency_exchange),
+                    prefixText: '\$',
+                    constraints: const BoxConstraints(minWidth: 500),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _description,
+                  decoration: inputDecoration.copyWith(
+                    labelText: 'Description',
+                    icon: const Icon(Icons.description),
+                    constraints: const BoxConstraints(minWidth: 500),
+                  ),
+                ),
+                const SizedBox(height: 32),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    OutlinedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Cancel'),
+                    ),
+                    const SizedBox(width: 16),
+                    FilledButton(
+                      onPressed: () async {
+                        if (!_formKey.currentState!.validate()) {
+                          return;
+                        }
+
+                        await onCreate?.call(
+                          _brand.text,
+                          _description.text,
+                          double.tryParse(_estimatedValue.text),
+                          int.tryParse(_quantity.text) ?? 1,
+                        );
+
+                        await Future.delayed(Duration.zero, () {
+                          Navigator.of(context).pop();
+                        });
+                      },
+                      child: const Text('Create'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
