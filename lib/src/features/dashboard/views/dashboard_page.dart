e19214ddc9c1f37295bbe104a0193073906adc0f
Dillon Fagan <dillonfagan@pm.me>
2023-07-05 15:56:53 -0400

    Remove scroll under effect from AppBar on desktop

--- lib/src/features/dashboard/views/dashboard_page.dart
+++ lib/src/features/dashboard/views/dashboard_page.dart
@@ -120,6 +120,7 @@ class _DashboardPageState extends State<DashboardPage> {
             title: Text(module.title),
             centerTitle: mobile,
             elevation: 0,
+            scrolledUnderElevation: isMobile(context) ? 1 : 0,
             // leading: IconButton(
             //   onPressed: () {
             //     final user = Provider.of<UserViewModel>(context, listen: false);
