ee48115599580683f56afc23f8a3b5045b0b31a9
Dillon Fagan <dillonfagan@pm.me>
2023-07-11 15:52:07 -0400

    Pull out ItemsCard

--- lib/src/features/inventory/widgets/inventory_details/inventory_details.widget.dart
+++ lib/src/features/inventory/widgets/inventory_details/inventory_details.widget.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:librarian_app/src/features/common/widgets/input_decoration.widget.dart';
 import 'package:librarian_app/src/features/inventory/widgets/dialogs/add_inventory_dialog.widget.dart';
-import 'package:librarian_app/src/utils/media_query.dart';
+import 'package:librarian_app/src/features/inventory/widgets/inventory_details/items_card/items_card.widget.dart';
 
 import '../../data/item.model.dart';
-import 'details_card_header.widget.dart';
 
 class InventoryDetails extends StatelessWidget {
   const InventoryDetails({
@@ -54,72 +53,19 @@ class InventoryDetails extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 32),
-        Card(
-          elevation: isMobile(context) ? 1 : 0,
-          margin: EdgeInsets.zero,
-          clipBehavior: Clip.antiAlias,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: Column(
-              children: [
-                DetailsCardHeader(
-                  title: 'Inventory',
-                  trailing: TextButton.icon(
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (context) => AddInventoryDialog(
-                          onCreate: onAddItems,
-                        ),
-                      );
-                    },
-                    icon: const Icon(Icons.add),
-                    label: const Text('Add items'),
-                  ),
-                  children: [
-                    Row(
-                      children: [
-                        Text('Available: $availableItems'),
-                        const SizedBox(width: 16),
-                        Text('Total: ${items.length}'),
-                      ],
-                    ),
-                  ],
-                ),
-                if (items.isNotEmpty) const Divider(),
-                if (items.isNotEmpty)
-                  ListView.separated(
-                    shrinkWrap: true,
-                    itemCount: items.length,
-                    itemBuilder: (context, index) {
-                      final item = items[index];
-
-                      return ListTile(
-                        leading:
-                            item.available ? checkedInIcon : checkedOutIcon,
-                        title: Text('#${item.number}'),
-                        trailing: Text(item.brand ?? 'Generic'),
-                      );
-                    },
-                    separatorBuilder: (BuildContext context, int index) {
-                      return const Divider();
-                    },
-                  ),
-              ],
-            ),
-          ),
+        ItemsCard(
+          items: items,
+          availableItemsCount: availableItems,
+          onAddItemsPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) => AddInventoryDialog(
+                onCreate: onAddItems,
+              ),
+            );
+          },
         ),
       ],
     );
   }
 }
-
-const checkedInIcon = Tooltip(
-  message: 'Available',
-  child: Icon(Icons.circle, color: Colors.green, size: 16),
-);
-
-const checkedOutIcon = Tooltip(
-  message: 'Checked out',
-  child: Icon(Icons.circle, color: Colors.amber, size: 16),
-);
