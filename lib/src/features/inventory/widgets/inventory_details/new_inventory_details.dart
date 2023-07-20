5f2c368a7f91bf8f1dce608b6c388c5498d815ac
Dillon Fagan <dillonfagan@pm.me>
2023-07-20 15:07:29 -0400

    Refactor InventoryDetails using listenable viewmodel pattern

---
+++ lib/src/features/inventory/widgets/inventory_details/new_inventory_details.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:librarian_app/src/features/common/widgets/input_decoration.widget.dart';
+import 'package:librarian_app/src/features/inventory/widgets/dialogs/add_inventory_dialog.widget.dart';
+import 'package:librarian_app/src/features/inventory/widgets/inventory_details/data/inventory_details.vm.dart';
+import 'package:librarian_app/src/features/inventory/widgets/inventory_details/items_card/items_card.widget.dart';
+
+class InventoryDetails extends StatelessWidget {
+  const InventoryDetails({super.key, required this.details});
+
+  final InventoryDetailsViewModel details;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextField(
+          controller: details.nameController,
+          decoration: inputDecoration.copyWith(
+            icon: const Icon(Icons.build),
+            labelText: 'Name',
+          ),
+          onChanged: (_) => details.announceChanges(),
+        ),
+        const SizedBox(height: 16),
+        TextField(
+          controller: details.spanishNameController,
+          decoration: inputDecoration.copyWith(
+            icon: const Icon(Icons.build),
+            labelText: 'Name (Spanish)',
+          ),
+          onChanged: (_) => details.announceChanges(),
+        ),
+        const SizedBox(height: 32),
+        ItemsCard(
+          items: details.items,
+          availableItemsCount: details.availableItems,
+          onAddItemsPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) => AddInventoryDialog(
+                onCreate: details.addItems,
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/src/features/inventory/widgets/inventory_details/new_inventory_details_pane.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:librarian_app/src/features/dashboard/widgets/panes/pane_header.widget.dart';
+import 'package:librarian_app/src/features/inventory/data/detailed_thing.model.dart';
+import 'package:librarian_app/src/features/inventory/data/inventory.vm.dart';
+import 'package:librarian_app/src/features/inventory/widgets/inventory_details/data/inventory_details.vm.dart';
+import 'package:librarian_app/src/features/inventory/widgets/inventory_details/new_inventory_details.dart';
+import 'package:provider/provider.dart';
+
+class InventoryDetailsPane extends StatelessWidget {
+  const InventoryDetailsPane({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<InventoryViewModel>(
+      builder: (context, inventory, child) {
+        return Card(
+          clipBehavior: Clip.antiAlias,
+          child: inventory.selectedId == null
+              ? const Center(child: Text('Inventory Details'))
+              : FutureBuilder<DetailedThingModel>(
+                  future: inventory.getThingDetails(id: inventory.selectedId!),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return Center(child: Text(snapshot.error.toString()));
+                    }
+
+                    if (!snapshot.hasData) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+
+                    final thingDetails = snapshot.data!;
+
+                    final details = InventoryDetailsViewModel(
+                      inventory: inventory,
+                      thingId: thingDetails.id,
+                      name: thingDetails.name,
+                      spanishName: thingDetails.spanishName,
+                      items: thingDetails.items,
+                      availableItems: thingDetails.available,
+                    );
+
+                    return Column(
+                      children: [
+                        ListenableBuilder(
+                          listenable: details,
+                          builder: (context, child) {
+                            return PaneHeader(
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Text(
+                                        details.name,
+                                        style: const TextStyle(fontSize: 24),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      if (details.hasUnsavedChanges) ...[
+                                        Text(
+                                          'Unsaved Changes',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .labelMedium!
+                                              .copyWith(
+                                                color: Colors.white
+                                                    .withOpacity(0.8),
+                                                fontStyle: FontStyle.italic,
+                                              ),
+                                        ),
+                                        const SizedBox(width: 8),
+                                      ],
+                                      IconButton(
+                                        onPressed: details.hasUnsavedChanges
+                                            ? details.save
+                                            : null,
+                                        icon: const Icon(Icons.save_rounded),
+                                        tooltip: 'Save',
+                                      ),
+                                      const SizedBox(width: 4),
+                                      IconButton(
+                                        onPressed: details.hasUnsavedChanges
+                                            ? details.discardChanges
+                                            : null,
+                                        icon: const Icon(Icons.cancel),
+                                        tooltip: 'Discard Changes',
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                        Expanded(
+                          child: SingleChildScrollView(
+                            child: Padding(
+                              padding: const EdgeInsets.all(16),
+                              child: InventoryDetails(details: details),
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+        );
+      },
+    );
+  }
+}
