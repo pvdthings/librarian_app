2cc8daed768b50854e4fb0eec8aa04543bb8f7d5
Dillon Fagan <dillonfagan@pm.me>
2023-08-29 22:00:23 -0400

    Improve styling with CheckboxField

--- lib/src/features/inventory/widgets/inventory_details/inventory_details.dart
+++ lib/src/features/inventory/widgets/inventory_details/inventory_details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:librarian_app/src/features/common/widgets/checkbox_field.dart';
 import 'package:librarian_app/src/features/common/widgets/input_decoration.widget.dart';
 import 'package:librarian_app/src/features/inventory/widgets/dialogs/add_inventory_dialog.widget.dart';
 import 'package:librarian_app/src/features/inventory/widgets/inventory_details/data/inventory_details.vm.dart';
@@ -31,15 +32,14 @@ class InventoryDetails extends StatelessWidget {
           ),
           onChanged: (_) => details.announceChanges(),
         ),
-        const SizedBox(height: 16),
-        CheckboxListTile(
-          title: const Text('Hidden'),
+        const SizedBox(height: 32),
+        CheckboxField(
+          title: 'Hidden',
           value: details.hiddenNotifier.value,
           onChanged: (bool? value) {
             details.hiddenNotifier.value = value ?? false;
             details.announceChanges();
           },
-          controlAffinity: ListTileControlAffinity.leading,
         ),
         const SizedBox(height: 32),
         ItemsCard(
