f27655fd74534201641bb671a22e60aa3fc9544d
Dillon Fagan <dillonfagan@pm.me>
2023-12-05 00:00:43 -0500

    WIP edit loan from modal

--- lib/src/features/loans/widgets/loan_details/loan_details.dart
+++ lib/src/features/loans/widgets/loan_details/loan_details.dart
@@ -44,72 +44,75 @@ class LoanDetails extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          TextField(
-            controller: TextEditingController(text: borrower?.name),
-            readOnly: true,
-            decoration: const InputDecoration(
-              icon: Icon(Icons.person_rounded),
-              labelText: 'Borrower',
-              border: OutlineInputBorder(),
-              constraints: BoxConstraints(maxWidth: 500),
-            ),
-          ),
+          Detail(label: 'Borrower', value: borrower!.name),
           const SizedBox(height: 32),
           ...things.map((thing) {
-            return Container(
-              margin: const EdgeInsets.only(bottom: 16),
-              child: TextField(
-                controller: TextEditingController(
-                    text: '${thing.name} #${thing.number}'),
-                readOnly: true,
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.build_rounded),
-                  labelText: 'Thing',
-                  border: OutlineInputBorder(),
-                  constraints: BoxConstraints(maxWidth: 500),
-                ),
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 16),
+              child: Detail(
+                label: 'Thing',
+                value: '#${thing.number} ${thing.name}',
               ),
             );
           }),
           const SizedBox(height: 16),
           Wrap(
-            spacing: 32,
             runSpacing: 16,
             children: [
-              TextField(
-                controller: TextEditingController(
-                    text: '${checkedOutDate.month}/${checkedOutDate.day}'),
-                readOnly: true,
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.calendar_month_rounded),
-                  labelText: 'Checked Out',
-                  border: OutlineInputBorder(),
-                  constraints: BoxConstraints(maxWidth: 200),
-                ),
+              Detail(
+                label: 'Checked Out',
+                value:
+                    '${checkedOutDate.month}/${checkedOutDate.day}/${checkedOutDate.year}',
               ),
-              TextField(
-                controller: TextEditingController(
-                    text: '${dueDate.month}/${dueDate.day}'),
-                readOnly: !editable,
-                onTap: () {
-                  if (editable) {
-                    showDateSelection(context);
-                  }
-                },
-                decoration: InputDecoration(
-                  icon: const Icon(Icons.calendar_month_rounded),
-                  iconColor: isOverdue ? Colors.orange : null,
-                  suffixIcon:
-                      editable ? const Icon(Icons.edit_calendar_rounded) : null,
-                  labelText: 'Due Back',
-                  border: const OutlineInputBorder(),
-                  constraints: const BoxConstraints(maxWidth: 200),
-                ),
+              const SizedBox(width: 16),
+              Detail(
+                label: 'Due Back',
+                value: '${dueDate.month}/${dueDate.day}/${dueDate.year}',
               ),
             ],
           ),
         ],
       ),
+    );
+  }
+}
+
+class Detail extends StatelessWidget {
+  const Detail({
+    super.key,
+    required this.label,
+    required this.value,
+  });
+
+  final String label;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 4),
+          child: Text(
+            label,
+            style: Theme.of(context).textTheme.labelMedium,
+          ),
+        ),
+        const SizedBox(height: 4),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+            color: Colors.black38,
+          ),
+          child: Text(
+            value,
+            style:
+                Theme.of(context).textTheme.bodyLarge!.copyWith(fontSize: 18),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/src/features/loans/widgets/loan_details/loan_details_pane.dart
+++ lib/src/features/loans/widgets/loan_details/loan_details_pane.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:librarian_app/src/features/common/widgets/save_dialog.widget.dart';
+import 'package:librarian_app/src/features/common/widgets/filled_progress_button.dart';
 import 'package:librarian_app/src/features/dashboard/widgets/panes/pane_header.widget.dart';
 
 import '../../models/loan_model.dart';
@@ -74,24 +74,22 @@ class _LoanDetailsPaneState extends State<LoanDetailsPane> {
                             const SizedBox(width: 8),
                           ],
                           IconButton(
-                            onPressed: _hasUnsavedChanges()
-                                ? () async {
-                                    if (await showSaveDialog(context)) {
-                                      widget.onSave(_newDueDate!);
-                                      setState(_reset);
-                                    }
-                                  }
-                                : null,
-                            icon: const Icon(Icons.save_rounded),
-                            tooltip: 'Save',
-                          ),
-                          const SizedBox(width: 4),
-                          IconButton(
-                            onPressed: _hasUnsavedChanges()
-                                ? () => setState(_reset)
-                                : null,
-                            icon: const Icon(Icons.cancel),
-                            tooltip: 'Discard Changes',
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return _EditLoanDialog(
+                                    dueDate: loan.dueDate,
+                                    isOverdue: false,
+                                    onSavePressed: (newDueDate) {
+                                      widget.onSave(newDueDate);
+                                    },
+                                  );
+                                },
+                              );
+                            },
+                            icon: const Icon(Icons.edit),
+                            tooltip: 'Edit',
                           ),
                           SizedBox(
                             height: 24,
@@ -139,6 +137,74 @@ class _LoanDetailsPaneState extends State<LoanDetailsPane> {
                 ),
               ],
             ),
+    );
+  }
+}
+
+class _EditLoanDialog extends StatefulWidget {
+  const _EditLoanDialog({
+    required this.dueDate,
+    required this.isOverdue,
+    required this.onSavePressed,
+  });
+
+  final DateTime dueDate;
+  final bool isOverdue;
+  final void Function(DateTime newDueDate) onSavePressed;
+
+  @override
+  State<_EditLoanDialog> createState() => _EditLoanDialogState();
+}
+
+class _EditLoanDialogState extends State<_EditLoanDialog> {
+  late DateTime dueDate = widget.dueDate;
+
+  bool _hasUnsavedChanges() => dueDate != widget.dueDate;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(false),
+          child: const Text('Cancel'),
+        ),
+        FilledProgressButton(
+          onPressed: _hasUnsavedChanges()
+              ? () {
+                  widget.onSavePressed(dueDate);
+                  Navigator.of(context).pop(true);
+                }
+              : null,
+          child: const Text('Save'),
+        ),
+      ],
+      icon: const Icon(Icons.edit),
+      title: const Text('Edit Loan'),
+      content: TextField(
+        controller: TextEditingController(
+          text: '${dueDate.month}/${dueDate.day}',
+        ),
+        onTap: () {
+          showDatePicker(
+            context: context,
+            initialDate: dueDate,
+            firstDate: widget.dueDate,
+            lastDate: widget.dueDate.add(const Duration(days: 14)),
+          ).then((value) {
+            if (value == null) return;
+            setState(() => dueDate = value);
+          });
+        },
+        decoration: InputDecoration(
+          icon: const Icon(Icons.calendar_month_rounded),
+          iconColor: widget.isOverdue ? Colors.orange : null,
+          labelText: 'Due Back',
+          border: const OutlineInputBorder(),
+          constraints: const BoxConstraints(maxWidth: 200),
+        ),
+      ),
+      contentPadding: const EdgeInsets.all(16),
     );
   }
 }
