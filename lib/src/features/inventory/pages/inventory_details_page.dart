6e2ddec0da508e520602974a64726cac2422ad01
Dillon Fagan <dillonfagan@pm.me>
2024-01-13 18:03:58 -0500

    Implement Delete Thing [mobile]

--- lib/src/features/inventory/pages/inventory_details_page.dart
+++ lib/src/features/inventory/pages/inventory_details_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:librarian_app/src/features/common/widgets/delete_dialog.dart';
 import 'package:librarian_app/src/features/inventory/providers/thing_details_provider.dart';
+import 'package:librarian_app/src/features/inventory/providers/things_repository_provider.dart';
 
 import '../providers/edited_thing_details_providers.dart';
+import '../providers/selected_thing_provider.dart';
 import '../widgets/inventory_details/inventory_details.dart';
 
 class InventoryDetailsPage extends ConsumerWidget {
@@ -26,28 +29,64 @@ class InventoryDetailsPage extends ConsumerWidget {
         final thingDetails = snapshot.data!;
         final hasUnsavedChanges = ref.watch(unsavedChangesProvider);
 
+        void discardChanges() async {
+          ref.read(thingDetailsEditorProvider).discardChanges();
+        }
+
+        Future<void> save() async {
+          await ref.read(thingDetailsEditorProvider).save();
+        }
+
+        Future<void> delete() async {
+          if (await showDeleteDialog(context,
+              title: 'Delete Thing',
+              message:
+                  'Are you sure you want to delete ${thingDetails.name}?\nThis action cannot be undone.')) {
+            ref.invalidate(selectedThingProvider);
+            ref
+                .read(thingsRepositoryProvider.notifier)
+                .deleteThing(thingDetails.id)
+                .whenComplete(() {
+              Navigator.of(context).pop();
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text('${thingDetails.name} deleted'),
+              ));
+            });
+          }
+        }
+
         return Scaffold(
           appBar: AppBar(
             title: Text(thingDetails.name),
             actions: [
               IconButton(
-                onPressed: hasUnsavedChanges
-                    ? ref.read(thingDetailsEditorProvider).save
-                    : null,
+                onPressed: hasUnsavedChanges ? save : null,
                 icon: const Icon(Icons.save),
               ),
               IconButton(
-                onPressed: hasUnsavedChanges
-                    ? ref.read(thingDetailsEditorProvider).discardChanges
-                    : null,
+                onPressed: hasUnsavedChanges ? discardChanges : null,
                 icon: const Icon(Icons.cancel),
               ),
             ],
           ),
-          body: const SingleChildScrollView(
+          body: SingleChildScrollView(
             child: Padding(
-              padding: EdgeInsets.all(16),
-              child: InventoryDetails(),
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  const InventoryDetails(),
+                  const SizedBox(height: 24),
+                  FilledButton.icon(
+                    onPressed: delete,
+                    icon: const Icon(Icons.delete_forever_outlined),
+                    label: const Text('Delete Thing'),
+                    style: FilledButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      foregroundColor: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         );
