c441c148286e13fb13695105190287698c743815
Dillon Fagan <dillonfagan@pm.me>
2024-02-13 08:23:13 -0500

    Introduce new loan details layout (WIP)

--- lib/src/features/loans/widgets/loan_details/loan_details.dart
+++ lib/src/features/loans/widgets/loan_details/loan_details.dart
@@ -24,112 +24,154 @@ class LoanDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final borrowerCard = Card(
+      elevation: 0,
+      child: Column(
+        children: [
+          Detail(
+            useListTile: true,
+            prefixIcon: const Icon(Icons.person),
+            label: 'Borrower',
+            value: borrower!.name,
+          ),
+          // const SizedBox(height: 16),
+          Detail(
+            useListTile: true,
+            prefixIcon: const Icon(Icons.email),
+            label: 'Borrower Email',
+            placeholderText: 'None',
+            value: borrower!.email,
+          ),
+          // const SizedBox(height: 16),
+          Detail(
+            useListTile: true,
+            prefixIcon: const Icon(Icons.phone),
+            label: 'Borrower Phone',
+            placeholderText: 'None',
+            value: borrower!.phone,
+          ),
+        ],
+      ),
+    );
+
+    final thingsCard = Card(
+      elevation: 0,
+      child: Column(
+        children: [
+          ...things.map((thing) {
+            return Detail(
+              useListTile: true,
+              prefixIcon: const Icon(Icons.build_rounded),
+              label: 'Thing',
+              value: '#${thing.number} ${thing.name}',
+            );
+          })
+        ],
+      ),
+    );
+
+    final datesCard = Card(
+      elevation: 0,
+      child: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        // mainAxisSize: MainAxisSize.min,
+        children: [
+          Detail(
+            useListTile: true,
+            prefixIcon: const Icon(Icons.calendar_month),
+            label: 'Checked Out',
+            value:
+                '${checkedOutDate.month}/${checkedOutDate.day}/${checkedOutDate.year}',
+          ),
+          Detail(
+            useListTile: true,
+            prefixIcon: Builder(
+              builder: (_) {
+                if (!isOverdue) {
+                  return const Icon(Icons.calendar_month);
+                }
+
+                return const Tooltip(
+                  message: 'Overdue',
+                  child: Icon(
+                    Icons.calendar_month,
+                    color: Colors.amber,
+                  ),
+                );
+              },
+            ),
+            label: 'Due Back',
+            value: '${dueDate.month}/${dueDate.day}/${dueDate.year}',
+          ),
+        ],
+      ),
+    );
+
     return SizedBox(
       width: double.infinity,
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Builder(builder: (context) {
-            final children = [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Detail(
-                    prefixIcon: const Icon(Icons.person),
-                    label: 'Borrower',
-                    value: borrower!.name,
-                  ),
-                  const SizedBox(height: 16),
-                  Detail(
-                    prefixIcon: const Icon(Icons.email),
-                    label: 'Borrower Email',
-                    placeholderText: 'None',
-                    value: borrower!.email,
-                  ),
-                  const SizedBox(height: 16),
-                  Detail(
-                    prefixIcon: const Icon(Icons.phone),
-                    label: 'Borrower Phone',
-                    placeholderText: 'None',
-                    value: borrower!.phone,
-                  ),
-                ],
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ...things.map((thing) {
-                    return Padding(
-                      padding: const EdgeInsets.only(bottom: 16),
-                      child: Detail(
-                        prefixIcon: const Icon(Icons.build_rounded),
-                        label: 'Thing',
-                        value: '#${thing.number} ${thing.name}',
-                      ),
-                    );
-                  })
-                ],
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Detail(
-                    prefixIcon: const Icon(Icons.calendar_month),
-                    label: 'Checked Out',
-                    value:
-                        '${checkedOutDate.month}/${checkedOutDate.day}/${checkedOutDate.year}',
-                  ),
-                  const SizedBox(height: 16),
-                  Detail(
-                    prefixIcon: Builder(
-                      builder: (_) {
-                        if (!isOverdue) {
-                          return const Icon(Icons.calendar_month);
-                        }
+          Wrap(
+            children: [
+              thingsCard,
+              datesCard,
+              borrowerCard,
+            ],
+          ),
+          // Builder(builder: (context) {
+          //   final children = [
+          //     Column(
+          //       crossAxisAlignment: CrossAxisAlignment.start,
+          //       children: [
+          //         Card(
+          //           elevation: 0,
+          //           child: Column(
+          //             children: [
+          //               ...things.map((thing) {
+          //                 return Detail(
+          //                   useListTile: true,
+          //                   prefixIcon: const Icon(Icons.build_rounded),
+          //                   label: 'Thing',
+          //                   value: '#${thing.number} ${thing.name}',
+          //                 );
+          //               })
+          //             ],
+          //           ),
+          //         ),
+          //       ],
+          //     ),
+          //   ];
 
-                        return const Tooltip(
-                          message: 'Overdue',
-                          child: Icon(
-                            Icons.calendar_month,
-                            color: Colors.amber,
-                          ),
-                        );
-                      },
-                    ),
-                    label: 'Due Back',
-                    value: '${dueDate.month}/${dueDate.day}/${dueDate.year}',
-                  ),
-                ],
-              ),
-            ];
+          //   if (isMobile(context)) {
+          //     return ListView.separated(
+          //       itemCount: children.length,
+          //       itemBuilder: (context, index) {
+          //         return children[index];
+          //       },
+          //       separatorBuilder: (context, index) {
+          //         return Container(
+          //           margin: const EdgeInsets.symmetric(vertical: 16),
+          //           child: const Divider(),
+          //         );
+          //       },
+          //       shrinkWrap: true,
+          //     );
+          //   }
 
-            if (isMobile(context)) {
-              return ListView.separated(
-                itemCount: children.length,
-                itemBuilder: (context, index) {
-                  return children[index];
-                },
-                separatorBuilder: (context, index) {
-                  return Container(
-                    margin: const EdgeInsets.symmetric(vertical: 16),
-                    child: const Divider(),
-                  );
-                },
-                shrinkWrap: true,
-              );
-            }
-
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: children,
-            );
-          }),
+          //   return Row(
+          //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //     crossAxisAlignment: CrossAxisAlignment.start,
+          //     children: children,
+          //   );
+          // }),
           Container(
             margin: const EdgeInsets.symmetric(vertical: 16),
             child: const Divider(),
           ),
           Detail(
+            useListTile: true,
             label: 'Notes',
             minWidth: 500,
             placeholderText: 'None',
--- lib/src/widgets/detail.dart
+++ lib/src/widgets/detail.dart
@@ -9,6 +9,7 @@ class Detail extends StatelessWidget {
     this.suffixIcon,
     this.minWidth,
     this.placeholderText,
+    this.useListTile = false,
   });
 
   final String label;
@@ -17,9 +18,28 @@ class Detail extends StatelessWidget {
   final Widget? suffixIcon;
   final double? minWidth;
   final String? placeholderText;
+  final bool useListTile;
 
   @override
   Widget build(BuildContext context) {
+    if (useListTile) {
+      return Container(
+        constraints: const BoxConstraints(
+          minWidth: 200,
+          maxWidth: 500,
+        ),
+        child: ListTile(
+          leading: prefixIcon,
+          trailing: suffixIcon,
+          title: Text(label),
+          titleTextStyle: Theme.of(context).textTheme.labelLarge,
+          subtitle: Text(value ?? placeholderText ?? ''),
+          subtitleTextStyle: Theme.of(context).textTheme.bodyLarge!.copyWith(
+              fontSize: 20, color: value == null ? Colors.white54 : null),
+        ),
+      );
+    }
+
     final text = Text(
       value ?? placeholderText ?? '',
       style: Theme.of(context)
@@ -36,29 +56,20 @@ class Detail extends StatelessWidget {
           style: Theme.of(context).textTheme.labelLarge,
         ),
         const SizedBox(height: 4),
-        Container(
-          constraints:
-              minWidth != null ? const BoxConstraints(minWidth: 300) : null,
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(8)),
-            color: Colors.black38,
-          ),
-          child: prefixIcon != null
-              ? Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    prefixIcon!,
+        prefixIcon != null
+            ? Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  prefixIcon!,
+                  const SizedBox(width: 8),
+                  text,
+                  if (suffixIcon != null) ...[
                     const SizedBox(width: 8),
-                    text,
-                    if (suffixIcon != null) ...[
-                      const SizedBox(width: 8),
-                      suffixIcon!,
-                    ],
+                    suffixIcon!,
                   ],
-                )
-              : text,
-        ),
+                ],
+              )
+            : text,
       ],
     );
   }
