37ecbe4554d2cc449a8222571aa0b7905f0a1ff2
Dillon Fagan <dillonfagan@pm.me>
2024-02-21 00:10:41 -0500

    Improve messaging re action duration

--- lib/src/features/actions/widgets/action_wizard_controller.dart
+++ lib/src/features/actions/widgets/action_wizard_controller.dart
@@ -40,25 +40,36 @@ class ActionWizardController extends ChangeNotifier {
   Future<void> _runAction() {
     isLoading = true;
     return service.extendAllDueDates(dueDate!).then((success) {
-      Future.delayed(Duration(seconds: success ? 5 : 0), () {
-        Navigator.of(context).pop(success);
+      Navigator.of(context).pop(success);
 
-        if (success) {
-          final dateString = formatDateForHumans(dueDate!);
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              content: Text('All due dates were updated to $dateString')));
-        } else {
-          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-            content: Row(
-              children: [
-                Icon(Icons.error, color: Colors.red),
-                SizedBox(width: 8),
-                Text('Whoops! Due dates could not be extended.'),
-              ],
-            ),
-          ));
-        }
-      });
+      if (success) {
+        final dateString = formatDateForHumans(dueDate!);
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          showCloseIcon: true,
+          content: Row(
+            children: [
+              Icon(
+                Icons.electric_bolt_rounded,
+                color: Theme.of(context).primaryColor,
+              ),
+              const SizedBox(width: 8),
+              Text(
+                  'Updating all dues dates to $dateString... This action may take several minutes to complete.'),
+            ],
+          ),
+          duration: const Duration(seconds: 15),
+        ));
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Row(
+            children: [
+              Icon(Icons.error, color: Colors.red),
+              SizedBox(width: 8),
+              Text('Whoops! Due dates could not be extended.'),
+            ],
+          ),
+        ));
+      }
     });
   }
 }
