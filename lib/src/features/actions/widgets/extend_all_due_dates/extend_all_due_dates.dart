78835824f7beb64b167f7637297081f17cd37d06
Dillon Fagan <dillonfagan@pm.me>
2024-02-20 23:57:20 -0500

    Update action messaging

--- lib/src/features/actions/widgets/extend_all_due_dates/extend_all_due_dates.dart
+++ lib/src/features/actions/widgets/extend_all_due_dates/extend_all_due_dates.dart
@@ -1,5 +1,5 @@
 class ExtendAllDueDates {
   static const String title = 'Extend All Due Dates';
   static const String description =
-      'Extends all loan due dates to a new specified date. This is very useful if open hours have been canceled.';
+      'Extends all loan due dates to a new specified date. This may be useful if open hours have been canceled.';
 }
