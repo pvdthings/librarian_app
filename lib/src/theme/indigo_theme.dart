a05564555a19ffbd78d4dd15b68d310daadf9dab
Dillon Fagan <dillonfagan@pm.me>
2024-03-06 20:36:45 -0500

    Improve theme for selected list tiles

--- lib/src/theme/indigo_theme.dart
+++ lib/src/theme/indigo_theme.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 
-final indigoTheme = ThemeData(
-  primarySwatch: Colors.indigo,
-  primaryColor: Colors.deepPurple,
-  brightness: Brightness.dark,
-  useMaterial3: true,
-);
+ThemeData _createIndigoTheme() {
+  final baseTheme = ThemeData(
+    primarySwatch: Colors.indigo,
+    primaryColor: Colors.deepPurple,
+    brightness: Brightness.dark,
+    useMaterial3: true,
+  );
+
+  return baseTheme.copyWith(
+    listTileTheme: ListTileThemeData(
+      selectedTileColor:
+          baseTheme.colorScheme.secondaryContainer.withAlpha(100),
+      selectedColor: Colors.white,
+    ),
+  );
+}
+
+final indigoTheme = _createIndigoTheme();
